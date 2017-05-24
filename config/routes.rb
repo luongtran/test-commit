f7c5223743fb64d18d69fcaff9c0be8963e3c464
Charles Ongaya <luongtranduc@gmail.com>
2017-05-24 09:07:12 +0700

    test
     commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TestCommit::Application.routes.draw do
+  resources :tests
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
