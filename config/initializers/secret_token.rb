0d90ff941b7a614da005e1b41ce4301bb6bd9544
Charles Ongaya <luongtranduc@gmail.com>
2017-05-24 08:56:07 +0700

    test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestCommit::Application.config.secret_key_base = 'a1f21ee0f09ab7fa4b5afc7be4035f89eba39114bfa8cb0f6d48d8f552a333306015230f4ae70e1cd9c5a735c5bf4d93656d04e6298c99f1aa76daf2ae571eb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestCommit::Application.config.session_store :cookie_store, key: '_testCommit_session'
